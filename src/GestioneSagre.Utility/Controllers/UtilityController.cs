18d21189f0939ddb9ed3e40dcc1339ca7d6455ac
Angelo Pirola <angelo9871@gmail.com>
2023-02-11 16:20:27 +0100

    Primo commit

---
+++ src/GestioneSagre.Utility/Controllers/UtilityController.cs
@@ -0,0 +1,64 @@
+﻿using GestioneSagre.Utility.Controllers.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestioneSagre.Utility.Controllers;
+
+public class UtilityController : BaseController
+{
+    private readonly ILogger<UtilityController> _logger;
+
+    public UtilityController(ILogger<UtilityController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetTipoCliente")]
+    public async Task<List<string>> GetTipoCliente()
+    {
+        _logger.LogInformation("GetTipoCliente");
+
+        var result = await Task.FromResult(new List<string> { "Cliente", "Staff" });
+
+        return result;
+    }
+
+    [HttpGet("GetTipoScontrino")]
+    public async Task<List<string>> GetTipoScontrino()
+    {
+        _logger.LogInformation("GetTipoScontrino");
+
+        var result = await Task.FromResult(new List<string> { "Pagamento", "Omaggio" });
+
+        return result;
+    }
+
+    [HttpGet("GetTipoPagamento")]
+    public async Task<List<string>> GetTipoPagamento()
+    {
+        _logger.LogInformation("GetTipoPagamento");
+
+        var result = await Task.FromResult(new List<string> { "Contanti", "Carta di credito", "Carta di debito" });
+
+        return result;
+    }
+
+    [HttpGet("GetScontrinoPagato")]
+    public async Task<List<string>> GetScontrinoPagato()
+    {
+        _logger.LogInformation("GetScontrinoPagato");
+
+        var result = await Task.FromResult(new List<string> { "Pagato", "Non pagato" });
+
+        return result;
+    }
+
+    [HttpGet("GetScontrinoStato")]
+    public async Task<List<string>> GetScontrinoStato()
+    {
+        _logger.LogInformation("GetScontrinoStato");
+
+        var result = await Task.FromResult(new List<string> { "Aperto", "In elaborazione", "Da incassare", "Chiuso", "Annullato" });
+
+        return result;
+    }
+}
---
+++ src/GestioneSagre.Utility/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace GestioneSagre.Utility;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("GestioneSagre.Utility", policy =>
+            {
+                policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(config =>
+        {
+            config.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Gestione Sagre Utility",
+                Version = "v1"
+            });
+        });
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        app.UseHttpsRedirection();
+        app.UseCors("GestioneSagre.Utility");
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Gestione Sagre Utility v1");
+        });
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
