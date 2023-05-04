705901fc83284d62f78517665a9824f33b982715
Angelo Pirola <angelo9871@gmail.com>
2023-05-04 18:57:20 +0200

    Aggiunto servizio di cache

---
+++ src/GestioneSagre.Utility/Core/Interfaces/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace GestioneSagre.Utility.Core.Interfaces;
+
+public interface ICacheService
+{
+    T Get<T>(string key);
+    T Set<T>(string key, T value, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null);
+}
---
+++ src/GestioneSagre.Utility/Core/Services/CacheService.cs
@@ -0,0 +1,39 @@
+﻿namespace GestioneSagre.Utility.Core.Services;
+
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _cache;
+
+    public CacheService(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public T Get<T>(string key)
+    {
+        var value = _cache.GetString(key);
+
+        if (value != null)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        return default;
+    }
+
+    public T Set<T>(string key, T value, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null)
+    {
+        var options = new DistributedCacheEntryOptions
+        {
+            // Imposta l'ora in cui la cache scadrà a partire dall'ora di inserimento (che rappresenta l'adesso)
+            AbsoluteExpirationRelativeToNow = absoluteExpireTime,
+
+            // Tempo fino al quale la voce della cache è valida, prima del quale se si verifica un hit il tempo deve essere esteso ulteriormente.
+            SlidingExpiration = slidingExpireTime
+        };
+
+        _cache.SetString(key, JsonConvert.SerializeObject(value), options);
+
+        return value;
+    }
+}
