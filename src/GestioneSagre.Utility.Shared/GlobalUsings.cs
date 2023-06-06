71df8bc3ad93d20fd15da8586c32e96a4bb305cc
Angelo Pirola <angelo9871@gmail.com>
2023-06-06 21:45:43 +0200

    Refactoring Shared

--- src/GestioneSagre.Utility.Shared/GlobalUsings.cs
+++ src/GestioneSagre.Utility.Shared/GlobalUsings.cs
@@ -1,2 +1,2 @@
-﻿global using GestioneSagre.Utility.Shared.Entities;
-global using NET6CustomLibrary.EFCore.Core.Interfaces;
+﻿global using CustomLibrary.EFCore.EFCore.Core.Interfaces;
+global using GestioneSagre.Utility.Shared.Entities;
